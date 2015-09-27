a25b35a423232b988d2de7353942e2f3bafe7a45
Aric Kampschaefer <a.kampschaefer@ou.edu>
2015-09-27 00:46:58 -0500

    added html parsing for euge.ca and updating the view accordingly

--- webcomic-viewer/ViewController.swift
+++ webcomic-viewer/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  webcomic-viewer
@@ -10,16 +11,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var comicTitle: UILabel!
+    @IBOutlet weak var comicImage: UIImageView!
+    var nextComic: String?
+    var previousComic: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let startingComic = "http://euge.ca/"
+        nextComic = startingComic
+        parseHTMLFromURL(startingComic)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Actions
 
+    @IBAction func getPrevious(sender: AnyObject) {
+        parseHTMLFromURL(previousComic!)
+    }
+
+    @IBAction func getNext(sender: AnyObject) {
+        parseHTMLFromURL(nextComic!)
+    }
+    
+    // MARK: - HTML Parsing
+    
+    func parseHTMLFromURL(currentComic: String) {
+
+        let HTMLData = NSData(contentsOfURL: NSURL(string: currentComic)!)!
+        
+        let parser = TFHpple(HTMLData: HTMLData)
+        // query for image
+        if let elements = parser.searchWithXPathQuery("//div[@class='entry']//img/@src") as? [TFHppleElement]{
+            for element in elements{
+                comicImage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: element.content)!)!)
+            }
+        }
+        // query for title
+        if let elements = parser.searchWithXPathQuery("//div[@class='post']/center/h2/a") as? [TFHppleElement]{
+            for element in elements{
+                comicTitle.text = element.content
+            }
+        }
+        // query for next comic
+        if let elements = parser.searchWithXPathQuery("//div[@class='alignright']/a/@href") as? [TFHppleElement]{
+            for element in elements{
+                nextComic = element.content
+                println(element.content)
+            }
+        }
+        // query for previous comic
+        if let elements = parser.searchWithXPathQuery("//div[@class='alignleft']/a/@href") as? [TFHppleElement]{
+            for element in elements{
+                previousComic = element.content
+                println(element.content)
+            }
+        }
+
+    }
 
 }
 
