5f91a33e7c6c6982b30de231194a7572c3602576
Aric Kampschaefer <a.kampschaefer@ou.edu>
2015-09-27 00:07:22 -0500

    Initial Commit

---
+++ webcomic-viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webcomic-viewer
+//
+//  Created by Aric Kampschaefer on 9/27/15.
+//  Copyright (c) 2015 Aric Kampschaefer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
